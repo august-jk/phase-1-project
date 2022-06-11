d224a549e0cb06201545b2c6a4c6782218247295
Aletha Wheeless <augustkama98@gmail.com>
2022-06-11 13:54:10 -1000

    add content to DOM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 } )
 const getCats = () => {
-    const url = 'https://api.thecatapi.com/v1/images/search?limit=100'
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
     fetch(url, {
         method: 'GET',
         headers: {
@@ -12,5 +12,30 @@ const getCats = () => {
     }
     })
     .then(resp => resp.json())
-    .then((array, cat) => array.foreach(console.log(cat)))
+    .then(createCard)
+}
+const createCard = (array) => {
+    array.forEach( cat => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        const cardContainer = document.querySelector('#cards');
+        cardContainer.appendChild(card);
+        card.innerHTML = `
+            <img src='${cat.url}' class='cat-img'/>
+            <div class="container">
+                <p>${cat.likes} Likes</p>
+                <button class="like-btn">♡</button>
+                <form action="submit" class="comment">
+                    <input type="text" placeholder='comment'></input>
+                    <input type="submit"></input>
+                </form>
+            </div>
+             ` 
+        const likeBtn = card.querySelector('.like-btn');
+        likeBtn.addEventListener('click', () => {
+            cat.likes += 1;
+            card.querySelector('p').textContent = `${cat.likes} Likes`
+            updateLikes(cat)
+    });
+    })
 }
