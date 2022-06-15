0dc174fab942463f8d163af730bfd7c0aefaa450
Aletha Wheeless <augustkama98@gmail.com>
2022-06-14 16:30:54 -1000

    accidentally deleted everything and then redid it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     getCats();
-
+    
 
 } )
 const getCats = () => {
@@ -12,41 +12,32 @@ const getCats = () => {
     }
     })
     .then(resp => resp.json())
-    .then(createCard)
+    .then(cat => cat.map(createCard))
 }
-const createCard = (array) => {
-    array.forEach( cat => {
-        cat.likes = 0;
-        const card = document.createElement('div');
-        card.className = 'card';
-        const cardContainer = document.querySelector('#cards');
-        cardContainer.appendChild(card);
-        card.innerHTML = `
-            <img src='${cat.url}' class='cat-img'/>
-            <div class='likes'>
-           <button class="like-btn"> ♥ </button><p> ${cat.likes} Likes</p>
-           </div>
-            <div class='comments'>
-            <h4>Comments:<h4>
-            <p></p>
-            </div>
-            <div class='form'>
-            <form action="submit"  class='comment-box'>
-                <input type="text" placeholder='comment'></input>
-                <input type="submit"></input>
-            </form>
-            </div>
-             ` 
-        const likeBtn = card.querySelector('.like-btn');
-        likeBtn.addEventListener('click', () => {
-            cat.likes += 1;
-            card.querySelector('p').textContent = `${cat.likes} Likes`
-            updateLikes(cat)
-                }
-            )
-        let form = document.querySelectorAll('.comment-box');
-        let formArr = Array.from(form);
-        formArr.forEach(item => item.addEventListener('submit', (e) => e.preventDefault))  
-        }
-    )
+const createCard = (cat) => {
+   let div = document.createElement('div');
+   div.className = 'card'
+   let cardsContainer = document.querySelector('#cards-container');
+   cat.likes = 0;
+   div.innerHTML = `
+   <img src='${cat.url}'/>
+   <div class='likes'>
+        <button class="like-btn"> ♥ </button><p> ${cat.likes} Likes</p>
+   </div>
+   <div class='comments'>
+        <h4>Comments:<h4>
+        <p></p>
+        </div>
+        <div class='comment-box'>
+        <form>
+            <input type="text" placeholder='comment'></input>
+            <input type="submit"></input>
+        </form>
+    </div>
+   `
+
+
+
+   cardsContainer.appendChild(div)
+    
 }
